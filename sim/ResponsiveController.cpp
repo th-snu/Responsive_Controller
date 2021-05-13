7c1abdafeaf83e776edbed750b29672e53f74995
th <54742258+th-snu@users.noreply.github.com>
2021-05-13 14:27:34 +0900

    Update ResponsiveController.cpp

--- sim/ResponsiveController.cpp
+++ sim/ResponsiveController.cpp
@@ -324,38 +324,38 @@ void ResponsiveController::
 	Eigen::VectorXd positions = skel->getPositions();
 	Eigen::VectorXd velocities = skel->getVelocities();
 
-	// while(!contact_timestamp.empty() && contact_timestamp.front() < this->mTimeElapsed - max_reaction_frame){
-	// 	contact_timestamp.erase(contact_timestamp.begin());
-	// 	d_expected_positions.erase(d_expected_positions.begin());
-	// 	d_expected_velocities.erase(d_expected_velocities.begin());
-	// }
+	while(!contact_timestamp.empty() && contact_timestamp.front() < this->mTimeElapsed - max_reaction_frame){
+		contact_timestamp.erase(contact_timestamp.begin());
+		d_expected_positions.erase(d_expected_positions.begin());
+		d_expected_velocities.erase(d_expected_velocities.begin());
+	}
 	
-	// Eigen::VectorXd position_bias(positions.size());
-	// Eigen::VectorXd velocity_bias(velocities.size());
-	// position_bias.setZero();
-	// velocity_bias.setZero();
+	Eigen::VectorXd position_bias(positions.size());
+	Eigen::VectorXd velocity_bias(velocities.size());
+	position_bias.setZero();
+	velocity_bias.setZero();
 
-	// for (int i = 0; i < contact_timestamp.size(); i++){
-	// 	int contactTimeElapsed = this->mTimeElapsed - contact_timestamp[i];
-	// 	double weight = 1.0 - (exp(contactTimeElapsed) - 1) / (exp(max_reaction_frame) - 1);
-	// 	cout << "dev: " << d_expected_velocities[i].norm() <<  ", weight: " << weight << endl;
-	// 	auto d_v = d_expected_velocities[i] * weight;
-	// 	velocity_bias += d_v;
-	// 	position_bias += d_expected_positions[i] * weight + (1.0 / mSimulationHz) * d_v * contactTimeElapsed;
-	// }
+	for (int i = 0; i < contact_timestamp.size(); i++){
+		int contactTimeElapsed = this->mTimeElapsed - contact_timestamp[i];
+		double weight = 1.0 - (exp(contactTimeElapsed) - 1) / (exp(max_reaction_frame) - 1);
+		cout << "dev: " << d_expected_velocities[i].norm() <<  ", weight: " << weight << endl;
+		auto d_v = d_expected_velocities[i] * weight;
+		velocity_bias += d_v;
+		position_bias += d_expected_positions[i] * weight + (1.0 / mSimulationHz) * d_v * contactTimeElapsed;
+	}
 
-	// positions += position_bias;
-	// velocities += velocity_bias;
-	// this->d_position_bias = position_bias - this->last_position_bias;
-	// auto d_velocity_bias = velocity_bias - this->last_velocity_bias;
-	// this->last_position_bias = position_bias;
-	// this->last_velocity_bias = velocity_bias;
+	positions += position_bias;
+	velocities += velocity_bias;
+	this->d_position_bias = position_bias - this->last_position_bias;
+	auto d_velocity_bias = velocity_bias - this->last_velocity_bias;
+	this->last_position_bias = position_bias;
+	this->last_velocity_bias = velocity_bias;
 
-	// vSkel->setPositions(skel->getPositions() + d_position_bias);
-	// vSkel->setVelocities(vSkel->getVelocities() + d_velocity_bias);
+	vSkel->setPositions(skel->getPositions() + d_position_bias);
+	vSkel->setVelocities(vSkel->getVelocities() + d_velocity_bias);
 
-	vSkel->setPositions(positions);
-	vSkel->setVelocities(velocities);
+	// vSkel->setPositions(positions);
+	// vSkel->setVelocities(velocities);
 
 	// auto collisionSolver = mVirtualWorld->getConstraintSolver();
 	// for(auto contact: collisionSolver->getLastCollisionResult().getContacts()){
